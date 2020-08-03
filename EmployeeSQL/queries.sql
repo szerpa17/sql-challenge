02bd3935e73aff09dc133b4e72693435b48b8dc4
szerp <szerp002@gmail.com>
2020-08-02 21:38:21 -0400

    employee gender by name query

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -22,7 +22,10 @@ SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.d
 	INNER JOIN departments on dept_emp.dept_no = departments.dept_no;
 	
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT first_name, last_name, sex
+	FROM employees
+	WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+	
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
