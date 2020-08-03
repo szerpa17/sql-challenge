e6f1280341e7fbbed29c85ef48ae592a0b38a16d
szerp <szerp002@gmail.com>
2020-08-02 21:20:12 -0400

    dept manager query

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -9,7 +9,11 @@ SELECT first_name, last_name, hire_date
 	WHERE hire_date >= '1986-01-01' AND  hire_date < '1987-01-01';
 	
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-SELECT dept_no, dept_name, the manager's employee number, last name, first name
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, 
+employees.last_name, employees.first_name
+	FROM dept_manager
+	INNER JOIN departments on departments.dept_no = dept_manager.dept_no
+	INNER JOIN employees on employees.emp_no = dept_manager.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
