20374eead5c480d10fe5d3aad97a867ca8e08ca3
szerp <szerp002@gmail.com>
2020-08-02 20:45:31 -0400

    separated table initialization code from queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,41 +1,3 @@
--- DROP TABLE IF EXISTS departments, titles, employees, dept_emp, dept_manager, salaries;
-
--- CREATE TABLE departments (
---     dept_no VARCHAR NOT NULL PRIMARY KEY,
---     dept_name VARCHAR NOT NULL);
-
--- CREATE TABLE titles (
---     title_id VARCHAR NOT NULL PRIMARY KEY,
---     title VARCHAR NOT NULL);
-	
--- CREATE TABLE employees (
---     emp_no INTEGER NOT NULL PRIMARY KEY,
---     emp_title VARCHAR NOT NULL,
---     birth_date DATE NOT NULL,
---     first_name VARCHAR NOT NULL,
---     last_name VARCHAR  NOT NULL,
---     sex VARCHAR NOT NULL,
---     hire_date DATE NOT NULL,
--- 	FOREIGN KEY(emp_title) REFERENCES titles(title_id));
-
--- CREATE TABLE dept_emp (
---     emp_no INTEGER NOT NULL ,
---     dept_no VARCHAR NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
--- 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no));
-
--- CREATE TABLE dept_manager (
---     dept_no VARCHAR NOT NULL,
---     emp_no INTEGER NOT NULL,
--- 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
-
--- CREATE TABLE salaries (
---     emp_no INTEGER NOT NULL,
---     salary INTEGER NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
-
-
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
 	FROM employees
@@ -47,6 +9,7 @@ SELECT first_name, last_name, hire_date
 	WHERE hire_date >= '1986-01-01' AND  hire_date < '1987-01-01';
 	
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dept_no, dept_name, the manager's employee number, last name, first name
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
