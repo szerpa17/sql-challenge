3deb38a1f72b05fd830f3cf24bd6a388321a628a
szerp <szerp002@gmail.com>
2020-08-02 20:34:24 -0400

    employee details query

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -34,4 +34,23 @@
 --     emp_no INTEGER NOT NULL,
 --     salary INTEGER NOT NULL,
 -- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
-	
+
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
+	FROM employees
+	INNER JOIN salaries on salaries.emp_no = employees.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
